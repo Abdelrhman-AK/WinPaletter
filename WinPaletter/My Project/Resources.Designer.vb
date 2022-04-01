5634ac07b75df1aaabc95aaa7e0ec68d71083373
Abdelrhman-AK <59510211+Abdelrhman-AK@users.noreply.github.com>
2022-04-02 00:56:27 +0200

    Alpha

--- WinPaletter/My Project/Resources.Designer.vb
+++ WinPaletter/My Project/Resources.Designer.vb
@@ -333,29 +333,9 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Public ReadOnly Property notify_info1() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("notify_info1", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Public ReadOnly Property notify_warning() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("notify_warning", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Public ReadOnly Property notify_warning1() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("notify_warning1", resourceCulture)
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
