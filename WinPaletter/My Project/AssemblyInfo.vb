68edc6218ff88044f2aa221a6723a4c239d4442e
Abdelrhman-AK <59510211+Abdelrhman-AK@users.noreply.github.com>
2022-11-13 19:59:04 +0200

    Update AssemblyInfo.vb

--- WinPaletter/My Project/AssemblyInfo.vb
+++ WinPaletter/My Project/AssemblyInfo.vb
@@ -30,5 +30,5 @@ Imports System.Runtime.InteropServices
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")>
 
-<Assembly: AssemblyVersion("1.0.6.2")>
-<Assembly: AssemblyFileVersion("1.0.6.2")>
+<Assembly: AssemblyVersion("1.0.6.3")>
+<Assembly: AssemblyFileVersion("1.0.6.3")>
