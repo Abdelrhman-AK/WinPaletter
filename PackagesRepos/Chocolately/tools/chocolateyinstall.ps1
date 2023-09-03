bc7ae927b0670bb718d6a6cdd82dac2a0e0784c3
Abdelrhman-AK <59510211+Abdelrhman-AK@users.noreply.github.com>
2023-09-03 17:16:37 +0300

    1.0.8.0

---
+++ PackagesRepos/Chocolately/tools/chocolateyinstall.ps1
@@ -0,0 +1,31 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+#$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$appdir = Join-Path -Path ${env:ProgramFiles} -ChildPath '\Abdelrhman-AK\WinPaletter'
+$desktopdir = [Environment]::GetFolderPath("Desktop")
+$startdir = [Environment]::GetFolderPath('CommonStartMenu') + "\Programs\WinPaletter"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  fileType      = 'EXE'
+  softwareName  = 'WinPaletter*'
+  unzipLocation = $appdir
+  url           = 'https://github.com/Abdelrhman-AK/WinPaletter/releases/download/v1.0.8.0/WinPaletter.zip'
+  checksum      = 'EF8A79746DA2E8D8273A39E9260EFFCE'
+  checksumType  = 'md5'
+}
+
+#Install-ChocolateyPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateypackage
+Install-ChocolateyZipPackage @packageArgs # https://docs.chocolatey.org/en-us/create/functions/install-chocolateyzippackage
+
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut($desktopdir + "\WinPaletter.lnk")
+$Shortcut.TargetPath = $appdir + "\WinPaletter.exe"
+$Shortcut.Arguments = ""
+$Shortcut.Save()
+
+New-Item -ItemType Directory -Force -Path $startdir
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut([Environment]::GetFolderPath('CommonStartMenu') + "\Programs\WinPaletter\WinPaletter.lnk")
+$Shortcut.TargetPath = $appdir + "\WinPaletter.exe"
+$Shortcut.Arguments = ""
+$Shortcut.Save()
---
+++ PackagesRepos/Chocolately/tools/chocolateyuninstall.ps1
@@ -0,0 +1,14 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+
+$appdir = Join-Path -Path ${env:ProgramFiles} -ChildPath '\Abdelrhman-AK\WinPaletter'
+$app = Join-Path -Path $appdir -ChildPath '\WinPaletter.exe'
+$desktoplnk = [Environment]::GetFolderPath("Desktop") + '\WinPaletter.lnk'
+$startdir = [Environment]::GetFolderPath('CommonStartMenu') + "\Programs\WinPaletter"
+
+Start-Process -FilePath $app -ArgumentList '/uninstall' -Wait
+
+Uninstall-ChocolateyZipPackage 'WinPaletter' 'WinPaletter.zip'
+
+if (Test-Path $appdir) {Remove-Item $appdir -verbose}
+if (Test-Path $startdir) {Remove-Item $startdir -verbose}
+if (Test-Path $desktoplnk) {Remove-Item $desktoplnk -verbose}
