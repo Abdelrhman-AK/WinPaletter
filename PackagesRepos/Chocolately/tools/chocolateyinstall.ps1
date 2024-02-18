f417d679f9c6bb8ce18fc97d4816f8f76b96fe6d
Abdelrhman-AK <dr.abdelrhman.ak@gmail.com>
2024-02-19 00:44:44 +0200

    1.0.8.5

--- PackagesRepos/Chocolately/tools/chocolateyinstall.ps1
+++ PackagesRepos/Chocolately/tools/chocolateyinstall.ps1
@@ -9,8 +9,8 @@ $packageArgs = @{
   fileType      = 'EXE'
   softwareName  = 'WinPaletter*'
   unzipLocation = $appdir
-  url           = 'https://github.com/Abdelrhman-AK/WinPaletter/releases/download/v1.0.8.4/WinPaletter.zip'
-  checksum      = '8342D34AEA526F414DD8773B7F836B0E'
+  url           = 'https://github.com/Abdelrhman-AK/WinPaletter/releases/download/v1.0.8.5/WinPaletter.zip'
+  checksum      = '4E07B81140A838336609547CE26560AA'
   checksumType  = 'md5'
 }
 
