ba789d3cf5c26fcc842de851bb679342914022cc
Abdelrhman-AK <dr.abdelrhman.ak@gmail.com>
2025-09-17 19:44:15 +0300

    1.0.9.6 - Fix critical Windows 10 issue

--- WinPaletter/Properties/AssemblyInfo.cs
+++ WinPaletter/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // <Assembly: AssemblyVersion("1.0.*")>
 
-[assembly: AssemblyVersion("1.0.9.5")]
-[assembly: AssemblyFileVersion("1.0.9.5")]
+[assembly: AssemblyVersion("1.0.9.6")]
+[assembly: AssemblyFileVersion("1.0.9.6")]
 [assembly: NeutralResourcesLanguage("")]
