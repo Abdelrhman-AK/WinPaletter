1208967f39a91b93e198c39444ac15564cad5e7b
Abdelrhman-AK <dr.abdelrhman.ak@gmail.com>
2025-09-23 21:51:28 +0300

    1.0.9.7

--- WinPaletter/Theme/Structures/Consoles/WinTerminal.cs
+++ WinPaletter/Theme/Structures/Consoles/WinTerminal.cs
@@ -724,7 +724,7 @@ namespace WinPaletter.Theme.Structures
                 {
                     if (reader.TokenType == JsonToken.Integer)
                     {
-                        int intValue = (int)reader.Value;
+                        int intValue = Convert.ToInt32(reader.Value);
 
                         // Check specific ranges and map them to suitable enum values
                         if (intValue >= 100 && intValue <= 200)
