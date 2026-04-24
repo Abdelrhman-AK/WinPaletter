858b739de79a2dfa40c9df6f4a05a598af7fe261
Abdelrhman-AK <dr.abdelrhman.ak@gmail.com>
2026-04-25 01:20:13 +0300

    1.0.9.9

--- WinPaletter/Tabs/TabsForm.cs
+++ WinPaletter/Tabs/TabsForm.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using WinPaletter.NativeMethods;
 
 namespace WinPaletter.Tabs
 {
@@ -8,8 +8,26 @@ namespace WinPaletter.Tabs
     /// </summary>
     public partial class TabsForm : UI.WP.Form
     {
-        private TitlebarControlHost _host;
-        private Control _placeholder;
+        protected override void WndProc(ref Message m)
+        {
+            base.WndProc(ref m);
+
+            // Handle the WM_NCCALCSIZE message to adjust the client area
+            if (m.Msg == 0x83 /*WM_NCCALCSIZE*/)
+            {
+                if (m.WParam.ToInt32() == 1 /*TRUE*/)
+                {
+                    // Get the current non-client area rectangle
+                    UxTheme.RECT rect = (UxTheme.RECT)m.GetLParam(typeof(UxTheme.RECT));
+
+                    // Adjust the rectangle to make room for the custom control in the title bar
+                    rect.top -= tabsContainer1.Height - 4;
+
+                    // Update the non-client area rectangle
+                    System.Runtime.InteropServices.Marshal.StructureToPtr(rect, m.LParam, true);
+                }
+            }
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="TabsForm"/> class.
@@ -17,24 +35,6 @@ namespace WinPaletter.Tabs
         public TabsForm()
         {
             InitializeComponent();
-        }
-
-        protected override void OnLoad(EventArgs e)
-        {
-            _host = new(this);
-            _placeholder = new();
-
-            _host.SetControl(_placeholder, 1);
-
-            base.OnLoad(e);
-        }
-
-        protected override void OnClosed(EventArgs e)
-        {
-            base.OnClosed(e);
-
-            _host?.Dispose();
-            _placeholder?.Dispose();
         }
     }
 }
